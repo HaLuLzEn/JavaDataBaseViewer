ed085d8e28ddb4009a163af21d0094e627e04214
HaLuLzEn <kinghalil667@gmail.com>
2024-04-25 23:14:52 +0200

    done nothing

---
+++ src/main/java/com/oda/Gui/ModifyPanel.java
@@ -0,0 +1,10 @@
+package com.oda.Gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ModifyPanel extends JPanel {
+    public ModifyPanel (String id, String table, Container cp) {
+        setLayout(null);
+    }
+}
